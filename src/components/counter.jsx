53954334ab4912f720aca773ce6baf70ee5ea508
Suvan Kaul <suvankaul97@gmail.com>
2020-02-22 19:44:12 +0530

    composing components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,26 +2,31 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
+        value: this.props.value,
         // imageUrl: 'https:picsum.photos/200'
         tags: ['tag1', 'tag2', 'tag3']
         // tags: []
     }
-
     // EVENT HANDLERS
     //this cant be used in the methods which are called on events (onClick) ie. it should be bind to the method. So we use constructor and bind "this" using ".bind" property
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        console.log(props)
         this.incrementCounter = this.incrementCounter.bind(this);
     }
-    incrementCounter(){
-        this.setState({count: this.state.count + 1})
-    }
+    // incrementCounter(){
+    //     this.setState({count: this.state.count + 1})
+    // }
 
     //another option is to convert the function into arrow function as it rebinds itself
-    decrementCounter = () =>{
+    incrementCounter = (product) => {
+        console.log(product);
+        this.setState({value: this.state.value + 1})
+    }
+    decrementCounter = (product) =>{
         //to change state property;
-        this.setState({count: this.state.count - 1})
+        console.log(product)
+        this.setState({value: this.state.value - 1})
     }
     productDetails = (product) =>{
         console.log(product)
@@ -32,8 +37,8 @@ class Counter extends Component {
         // background: 'green'
     }
     formatCounter() {
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
+        const { value } = this.state;
+        return value === 0 ? "Zero" : value;
     }
     renderTags() {
         if (this.state.tags.length === 0) return <p>There are no tags</p>;
@@ -49,15 +54,16 @@ class Counter extends Component {
         return (
             <div>
                 {/* <img src={this.state.imageUrl} alt=""></img> */}
+                {/* {this.props.children} */}
                 <span style={this.style} className={this.getBadgeClasses()}>{this.formatCounter()}</span>
-                <button style={{ 'fontSize': '16px','padding':'5px','margin':'5px' }} className="btn btn-secondary btn-sm" onClick={this.incrementCounter}>Counter Increment</button>
+                <button style={{ 'fontSize': '16px','padding':'5px','margin':'5px' }} className="btn btn-secondary btn-sm" onClick={() => this.incrementCounter("product")}>Counter Increment</button>
                 {/* To pass parameters into function */}
-                <button onClick={()=> this.productDetails("product")}>Product</button>
-                <button style={{ 'fontSize': '16px', 'padding':'5px','margin':'5px'}} className="btn btn-secondary btn-sm" onClick={this.decrementCounter}>Counter Decrement</button>
-                <div>
+                {/* <button onClick={()=> this.productDetails("product")}>Product</button> */}
+                <button style={{ 'fontSize': '16px', 'padding':'5px','margin':'5px'}} className="btn btn-secondary btn-sm" onClick={() => this.decrementCounter("product")}>Counter Decrement</button>
+                {/* <div>
                     {this.state.tags.length ===0 && "Please create Tags"}
                     {this.renderTags()}
-                </div>
+                </div> */}
             </div>
         )
     }
@@ -65,7 +71,7 @@ class Counter extends Component {
     //use refractor to convert loc into function (extract into methods of class)
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 }
